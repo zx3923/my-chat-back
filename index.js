c235ed89a53e29017cbfc7483acd9400b39a1c8b
zx3923 <seop3923@gmail.com>
2023-01-04 14:34:12 +0900

    배포수정

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const http = require("http").Server(app);
 const PORT = 4000;
 const socketIO = require("socket.io")(http, {
   cors: {
-    origin: "https://zx3923.github.io/my-chat-front/",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
